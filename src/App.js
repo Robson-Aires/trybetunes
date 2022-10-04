098d4aeddf9e03651fd189782bdff9f1eac06737
Robson Aires <robsonaires@outlook.com.br>
2022-10-04 17:42:08 -0300

    pedido de teste mentoria

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Login from './pages/login';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
@@ -10,15 +11,16 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <Login />
-        <Search />
-        <Album />
-        <Favorites />
-        <Profile />
-        <ProfileEdit />
-        <NotFound />
-      </>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+
+      </Switch>
     );
   }
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+// import Search from './Search';
+
+ class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      ArtistName: '',
+      loading: false,
+    };
+  }
+
+  handleChangeOfName = (event) => {
+    this.setState({
+      ArtistName: event.target.value,
+    }, () => {
+      const { ArtistName } = this.state;
+      const lenghtArtistName = ArtistName.length;
+      const NUMBER_OF_CHARACTERS = 3;
+      if (lenghtArtistName >= NUMBER_OF_CHARACTERS) {
+        this.setState({
+          isDisabled: false,
+        });
+      } else {
+        this.setState({
+          isDisabled: true,
+        });
+      }
+    });
+  };
+
+  xablau = async () => {
+    const { ArtistName, loading } = this.state;
+    this.setState(!loading);
+    const { history } = this.props;
+    await createUser({ name: ArtistName });
+    history.push('/Search');
+  };
+
+  render() {
+    const { isDisabled, ArtistName, loading } = this.state;
+    return (
+      {loading ? (<p>Carregando...</p>) : (
+      <div data-testid="page-login">
+        <input
+          value={ ArtistName }
+          type="text"
+          name="ArtistName"
+          data-testid="login-name-input"
+          onChange={ (event) => this.handleChangeOfName(event) }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+          onClick={ this.xablau }
+        >
+          Entrar
+        </button>
+      </div>
+      )}
+    );
+  }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+export default Login;
